14d21e50670eecf40261e506555f4d47c9ac4cda
Ryan Norris <nigeluno@norrisenterprises.org>
2014-05-08 19:53:09 -0400

    Some more shit I worked on a couple weeks ago. And by couple, I mean a while ago. Maybe @drewwalton19216801 can get this shit working.

--- OpenCannibal/Samsung.Designer.vb
+++ OpenCannibal/Samsung.Designer.vb
@@ -22,16 +22,28 @@ Partial Class Samsung
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(159, 336)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(169, 56)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Samsung
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(975, 532)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Samsung"
         Me.Text = "Samsung"
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
--- OpenCannibal/Samsung.vb
+++ OpenCannibal/Samsung.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Samsung
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start("chrome.exe")
+
+    End Sub
 End Class
